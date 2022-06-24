bd88f267eae9e2735b9110d49336afd443ea9775
joshuapreciado <preciadojoshua340@yahoo.com>
2022-06-23 20:57:52 -0700

    CFixed jcasing

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,12 +1,12 @@
-var ContainerBulb = document.querySelector('.container');
+var containerbulb = document.querySelector('.container');
 var bulb = document.querySelector('.light-button');
 bulb.addEventListener('click', lightBulb);
 function lightBulb(event) {
   if (bulb.className === 'light-button circle') {
     bulb.className = 'light-button ' + 'black';
-    ContainerBulb.className = 'container ' + 'black';
+    containerbulb.className = 'container ' + 'black';
   } else {
     bulb.className = 'light-button ' + 'circle';
-    ContainerBulb.className = 'container ' + 'white';
+    containerbulb.className = 'container ' + 'white';
   }
 }
