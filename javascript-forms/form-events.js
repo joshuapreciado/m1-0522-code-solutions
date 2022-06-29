46cdd1132703e4733165424006d253079bb09b0a
joshuapreciado <preciadojoshua340@yahoo.com>
2022-06-28 17:02:00 -0700

    Need help

--- javascript-forms/form-events.js
+++ javascript-forms/form-events.js
@@ -13,10 +13,20 @@ function handleInput(event) {
   console.log(event.target.value);
 }
 
-var test1 = document.querySelector('input');
-var test2 = document.querySelector('input');
-var test3 = document.querySelector('textarea');
+var test1 = document.querySelector('#user-name');
+var test2 = document.querySelector('#user-email');
+var test3 = document.querySelector('#user-message');
 
 test1.addEventListener('focus', handleFocus);
+test1.addEventListener('blur', handleBlur);
+test1.addEventListener('input', handleInput);
+
+// For blur
+test2.addEventListener('focus', handleFocus);
 test2.addEventListener('blur', handleBlur);
+test2.addEventListener('input', handleInput);
+
+// For Input
+test3.addEventListener('focus', handleFocus);
+test3.addEventListener('blur', handleBlur);
 test3.addEventListener('input', handleInput);
--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -1,6 +1,9 @@
 var test1 = document.querySelector('#contact-form');
 test1.addEventListener('submit', function (event) {
   event.preventDefault();
-  var test = document.getElementById('contact-form').elements;
-  console.log(test);
+  var test = document.getElementById('contact-form').elements.name.value;
+  var test2 = document.getElementById('contact-form').elements.email.value;
+  var test3 = document.getElementById('contact-form').elements.message.value;
+  var sum = test + test2 + test3;
+  console.log(sum);
 });
