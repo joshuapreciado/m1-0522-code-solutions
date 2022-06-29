97d78cd962c5041850901a84d77758c3d4cdf394
joshuapreciado <preciadojoshua340@yahoo.com>
2022-06-28 17:41:37 -0700

    Completed forms

--- javascript-forms/form-values.js
+++ javascript-forms/form-values.js
@@ -2,8 +2,15 @@ var test1 = document.querySelector('#contact-form');
 test1.addEventListener('submit', function (event) {
   event.preventDefault();
   var test = document.getElementById('contact-form').elements.name.value;
+  console.log('test:', test);
   var test2 = document.getElementById('contact-form').elements.email.value;
+  console.log('test:', test2);
   var test3 = document.getElementById('contact-form').elements.message.value;
-  var sum = test + test2 + test3;
-  console.log(sum);
+  console.log('test:', test3);
+  var dataValue = {
+    name: test,
+    email: test2,
+    message: test3
+  };
+  console.log('Value of Data Value: ', dataValue);
 });
